b5ce6d5f1afb7dd7bc5bc041da8071d1a4e3be04
alucardthefish <sergiop18@gmail.com>
2021-06-03 19:26:41 -0500

    Add progress bar for updating and adding book records

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -15,6 +15,9 @@ export class BooksComponent implements OnInit {
   isHide: Boolean;
   isNotUpdating: Boolean;
   bookId: String;
+  counter: number;
+  isNotRequesting: Boolean;
+  styleColor: String;
 
   //public books: Array<any> = []
   books: Book[];
@@ -24,6 +27,9 @@ export class BooksComponent implements OnInit {
 
       this.isHide = true;
       this.isNotUpdating = true;
+      this.counter = 10;
+      this.isNotRequesting = true;
+      this.styleColor = "info";
 
       this.form = this.fb.group({
         title: ['', [Validators.required, Validators.minLength(2)]],
@@ -53,18 +59,34 @@ export class BooksComponent implements OnInit {
   }
 
   submitForm() {
-    
+    this.isNotRequesting = false;
+    this.styleColor = "info"
     var newBook: any = new Book();
     newBook.title = this.form.get('title').value;
     newBook.author = this.form.get('author').value;
     newBook.read = this.form.get('read').value;
+    this.setProgress(50);
 
     this.bookService.addBook(newBook).subscribe((resp: any) => {
-      console.log(resp);
       this.books.push(resp);
-      this.form.reset();
+      this.form.reset({
+        title: '',
+        author: '',
+        read: false
+      });
+      this.setProgress(100);
     })
 
+  }
+
+  setProgress(prc: number) {
+    this.counter = prc;
+    if (prc == 100) {
+      setTimeout(() => {
+        this.isNotRequesting = true;
+        this.counter = 10;
+      }, 2000);
+    }
   }
 
   toggleButton() {
@@ -74,16 +96,23 @@ export class BooksComponent implements OnInit {
     }
   }
 
+  pushCancel() {
+    console.log("cancelar")
+    this.isNotUpdating = true;
+  }
+
   deleteBook(id: string) {
     console.log(`delete book with id: ${id}`);
     this.bookService.delete(id).subscribe((resp: any) => {
       console.log(resp);
       this.loadBooks();
     });
+    this.pushCancel();
+    alert("Book deleted successfully");
   }
 
   setUpdateBook(book: Book) {
-    console.log(book);
+    
     this.isNotUpdating = false;
     if (!this.isHide) {
       this.isHide = !this.isHide;
@@ -92,22 +121,31 @@ export class BooksComponent implements OnInit {
     this.updateForm.patchValue(book);
     this.bookId = book.id;
 
+    window.location.href = '#update-form';
 
   }
 
   updateBook() {
 
+    this.isNotRequesting = false;
+    this.styleColor = "warning"
     var updBook: any = new Book();
     updBook.id = this.bookId;
     updBook.title = this.updateForm.get('title').value;
     updBook.author = this.updateForm.get('author').value;
     updBook.read = this.updateForm.get('read').value;
+    console.log(updBook);
+    this.setProgress(50);
 
-    console.log("Book a modificar con id: " + this.bookId);
     this.bookService.update(updBook).subscribe((resp: any) => {
-      console.log(resp);
+      this.updateForm.reset({
+        title: '',
+        author: '',
+        read: false
+      });
       this.isNotUpdating = true;
       this.loadBooks();
+      this.setProgress(100);
     });
 
   }
